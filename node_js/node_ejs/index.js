f5fb45ee78b9b6a7d7f5542e1612963f0ae8bb9a
sunil dhayal <sunildhayal368@gmail.com>
2025-01-21 23:24:27 +0530

    learning ejs

---
+++ node_js/node_ejs/index.js
@@ -0,0 +1,61 @@
+/*
+ EJS(embedded javascript templates)
+
+ -> it is simple templating language that lets you generate HTML markup with plain javascript 
+
+*/
+// require("./views/home.ejs")
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"/views"));
+
+// start server listen for request at a particular port
+
+const port = 8080;
+
+
+
+app.listen(port,()=>{
+     console.log(`listening on the ${port}`)
+})
+
+
+
+// app.get("/",(req,res)=>{
+//     console.log("request received")
+//      res.send("hello this is home");
+// })
+
+
+/**
+  we can also provide the ejs file on request and ejs should be in the views directory 
+ */
+
+// app.get("/",(req,res)=>{
+//       res.render("home")
+// })
+
+/**
+ interpolation syntax
+   
+   interpolation refer to embedding expression into marked up text
+    example <%= %>
+
+ */
+
+ /**
+   
+ passing data to ejs 
+
+ */   
+
+app.get("/rolldice" , (req,res)=>{
+      let data_from_database = Math.floor(Math.random()*6)+1;
+      res.render("rolldice",{data_from_database});
+})
+
